b3c062573477289ad0b808ba6242556ee7341411
liminat <180078053+liminat@users.noreply.github.com>
2023-08-05 13:41:02 +0000

    Automated push of generated files  [ci skip]

--- CollaborativeBuilding/builder/model.py
+++ CollaborativeBuilding/builder/model.py
@@ -63,4 +63,60 @@ class UtteranceEncoder(nn.Module):
         packed = pack_padded_sequence(embedded, [seq_len]*batch_size, batch_first=True)
         output_packed, hidden = self.rnn(packed) 
         output_unpacked, lens_unpacked = pad_packed_sequence(output_packed, batch_first=True)
-        output = output_unpacked.view(batch_size, seq_len, 2 if self.bidirectional else 1 , self.rnn_hidden_
+        output = output_unpacked.view(batch_size, seq_len, 2 if self.bidirectional else 1 , self.rnn_hidden_size)
+        output = torch.mean(output, dim=-2)
+        return output # [batch_size, max_length, 300]
+
+    def init_weights(self):
+        """ Initializes weights of linear layers with Xavier initialization. """
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                m.bias.data.zero_()
+                nn.init.xavier_uniform_(m.weight)
+
+    def take_last_hidden(self, hidden, num_hidden_layers, bidirectional, batch_size, rnn_hidden_size):
+        hidden = hidden.view(num_hidden_layers, bidirectional, batch_size, rnn_hidden_size) # (num_layers, num_directions, batch, hidden_size)
+        hidden = hidden[-1] # hidden: (num_directions, batch, hidden_size)
+        return hidden
+
+
+class WorldStateEncoderCNN(nn.Module):
+    def __init__(self, config):
+        super(WorldStateEncoderCNN, self).__init__()
+        self.num_conv_layers = config['num_conv_layers']
+        self.world_dim = config['world_dim']
+        self.kernel_size = config['kernel_size']
+        self.input_size = config['world_hidden_size']
+
+        self.conv_layers = []
+        for i in range(self.num_conv_layers):
+            if i == 0:
+                layer = nn.Conv3d(self.world_dim, self.input_size, kernel_size=self.kernel_size, stride=1, padding=1)
+            else:
+                layer = nn.Conv3d(self.input_size, int(self.input_size/2), kernel_size=1, stride=1, padding=0)
+                self.conv_layers.append(layer)
+                layer = nn.Conv3d(int(self.input_size/2), self.input_size, kernel_size=self.kernel_size, stride=1, padding=1)
+            self.conv_layers.append(layer)
+        self.conv_layers = nn.ModuleList(self.conv_layers)
+        self.nonlinearity = nn.ReLU()
+        self.dropout = nn.Dropout3d(p=0.2)
+
+    def forward(self, world_repr):
+        """
+        input: [1, 8, 11, 9, 11]
+        output: [1, 300, 11, 9, 11]
+        """
+        for i, conv in enumerate(self.conv_layers):
+            world_repr = self.dropout(self.nonlinearity(conv(world_repr)))
+        return world_repr.permute(0, 2, 3, 4, 1).view(-1, 1089, self.input_size)
+
+
+class ActionDecoder(nn.Module):
+    def __init__(self, config):
+        super(ActionDecoder, self).__init__()
+        self.text_hidden_size = config['text_hidden_size']
+        self.text_dropout = config['text_dropout']
+        self.text_attn_heads = config['text_attn_heads']
+        self.text_layers = config['text_layers']
+
+        self.world_hidden_size = c
