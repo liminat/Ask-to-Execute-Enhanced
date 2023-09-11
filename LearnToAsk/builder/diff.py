df2253a05a346111eec7f072e88a4e629406f28f
liminat <180078053+liminat@users.noreply.github.com>
2023-09-11 15:16:30 +0000

    adding search

--- LearnToAsk/builder/diff.py
+++ LearnToAsk/builder/diff.py
@@ -84,4 +84,44 @@ def get_diff(gold_config, built_config, optimal_alignment=None):
     perturbations = generate_perturbations(built_config, gold_config = gold_config, optimal_alignment = optimal_alignment)
 
     # compute diffs for each perturbation
-    diffs = list([diff(go
+    diffs = list([diff(gold_config = gold_config, built_config = t.perturbed_config) for t in perturbations])
+
+    # convert diffs back to actions in the built config space and not the perturbed config space
+    # filter out perturbations that yield infeasible diff actions (those outside the build region)
+    perturbations_and_diffs = list([x for x in list(zip(perturbations, diffs)) if is_feasible_perturbation(x[0], x[1])])
+
+    # recompute diffs in gold config space
+    # orig_diffs = list([diff(gold_config = gold_config, built_config = x[0].perturbed_config) for x in perturbations_and_diffs])
+    # perturbations_diffs_and_orig_diffs = [x + (y,) for x, y in zip(perturbations_and_diffs, orig_diffs)]
+    perturbations_and_diffs = list([(x[0], Diff(diff_built_config_space = x[1], diff_gold_config_space = None)) for x in perturbations_and_diffs])
+
+    # select perturbation with min diff
+    min_perturbation_and_diff = min(perturbations_and_diffs, key = lambda t: len(t[1].diff_built_config_space["gold_minus_built"]) + len(t[1].diff_built_config_space["built_minus_gold"]))
+
+    # get all minimal diffs
+    diff_sizes = list([len(t[1].diff_built_config_space["gold_minus_built"]) + len(t[1].diff_built_config_space["built_minus_gold"]) for t in perturbations_and_diffs])
+    min_diff_size = min(diff_sizes)
+
+    perturbations_and_diffs_and_diff_sizes = list(zip(perturbations_and_diffs, diff_sizes))
+    perturbations_and_minimal_diffs_and_diff_sizes = list([x for x in perturbations_and_diffs_and_diff_sizes if x[1] == min_diff_size])
+
+    # reformat final output
+    perturbations_and_minimal_diffs = list([PerturbedConfigAndDiff(perturbed_config=x[0][0], diff=x[0][1]) for x in perturbations_and_minimal_diffs_and_diff_sizes])
+
+    return min_perturbation_and_diff[1].diff_built_config_space, perturbations_and_minimal_diffs
+
+def is_feasible_perturbation(perturbed_config, diff):
+    # NOTE: This function mutates `diff`. DO NOT CHANGE THIS BEHAVIOR!
+    """
+    Args:
+        perturbed_config: PerturbedConfig
+        diff: Dict
+    """
+
+    def find_orig_block(block, block_pairs):
+        return next(x[1] for x in block_pairs if x[0] == block)
+
+    for key, diff_config in list(diff.items()):
+        if key == "built_minus_gold": # retrieve from original built config instead of applying inverse transform
+            block_pairs = list(zip(perturbed_config.perturbed_config, perturbed_config.original_config))
+            diff[key] = list([find_orig_block(x, block_pairs) for x in 
